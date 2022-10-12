a03fcd34e22d496429ae7b08e325b9acd9ef3b55
Sergiok12 <sergioabd460@gmail.com>
2022-10-12 22:01:43 +0200

    Vista estatica generada

---
+++ lib/views/listview_views.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ListviewSceeen extends StatelessWidget {
+  final lista = ["Sergio", "Tatiana", "Rafaela", "Lucy", "Aldo", "Minimo"];
+  ListviewSceeen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Listview"),
+      ),
+      body: ListView(children:  [
+      ...lista.map(
+        (nombres) => ListTile(
+          title: Text(nombres),
+          trailing: const Icon(Icons.arrow_forward_ios_outlined),
+        )).toList()
+      ],)
+      
+    );
+  }
+}
