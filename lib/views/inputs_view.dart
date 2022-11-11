49700abc9d4ae29a344906beb181c17ca35305e1
Sergioabd460 <sergio@all-win.software>
2022-11-11 16:40:30 -0700

    agregando formulario basico

--- lib/views/inputs_view.dart
+++ lib/views/inputs_view.dart
@@ -4,8 +4,18 @@ import '../widgets/widgets.dart';
 class InputsView extends StatelessWidget {
   const InputsView({super.key});
 
+
   @override
   Widget build(BuildContext context) {
+
+    final GlobalKey <FormState> myFormKey = GlobalKey<FormState>();
+  Map<String, String> formvalues = {
+    'nombre' : 'Sergio',
+    'apellido' : 'duarte',
+    'correo' : 'Sergio@test.com',
+    'contra': '1234',
+    'role': 'Admin'
+    };
     return Scaffold(
       appBar: AppBar(
         title: const Text("Entradas"),
@@ -13,10 +23,33 @@ class InputsView extends StatelessWidget {
       body:  SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10,),
-          child: Column(
-              children: const [
-                 CustomImputWidget()
-              ],
+          child: Form(
+            key: myFormKey,
+            child: Column(
+                children:  [
+                   const CustomImputWidget( labelText: "Nombre del usuario",),
+                   const SizedBox(height: 30,),
+                   const CustomImputWidget( labelText: "Apellido del usuario",),
+                   const SizedBox(height: 30,),
+                   const CustomImputWidget( labelText: "Correo del usuario", keytipe: TextInputType.emailAddress,),
+                   const SizedBox(height: 30,),
+                   const CustomImputWidget( labelText: "Contraseña", isPassword: true,),
+                   const SizedBox(height: 30,),
+          
+                   ElevatedButton(
+                     child: const SizedBox(
+                      width: double.infinity,
+                      child: Text("OK", textAlign: TextAlign.center,)),
+                     onPressed: (){
+                      if(!myFormKey.currentState!.validate()){
+                        return;
+                      }
+                      debugPrint(formvalues.toString());
+                     }   
+                   ),
+          
+                ],
+            ),
           ),
           ),
       )
--- lib/widgets/custom_input.dart
+++ lib/widgets/custom_input.dart
@@ -1,45 +1,48 @@
 import 'package:flutter/material.dart';
-class CustomImputWidget extends StatelessWidget {
 
+class CustomImputWidget extends StatelessWidget {
   final String? hintText;
   final String? helperText;
   final String? labelText;
   final IconData? icon;
   final IconData? suffixIcon;
+  final TextInputType? keytipe;
+  final bool isPassword;
 
-  const CustomImputWidget({
-    Key? key,
-    this.hintText,
-    this.helperText,
-    this.labelText,
-    this.icon,
-    this.suffixIcon,
-  }) : super(key: key);
+  const CustomImputWidget(
+      {Key? key,
+      this.hintText,
+      this.helperText,
+      this.labelText,
+      this.icon,
+      this.suffixIcon,
+      this.isPassword = false,
+      this.keytipe})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      keyboardType: keytipe,
+      obscureText: isPassword,
       //autofocus: true,
       //initialValue: "Sergio Duarte",
       textCapitalization: TextCapitalization.words,
-      onChanged: (value){
+      onChanged: (value) {
         debugPrint(value);
       },
-      validator: (value){
-        if(value == null){
-          return "Este campo es requerido";
-        }
-        return "XD";
+      validator: (value) {
+        if (value == null) return "Este campo es requerido";
+        return value.length < 3 ? "Minimo 3 letras" : null;
       },
       autovalidateMode: AutovalidateMode.onUserInteraction,
-      
-      decoration:  InputDecoration(
-        icon: Icon(icon),
-        suffixIcon: Icon(suffixIcon),
-        hintText: hintText,
-        labelText: labelText,
-        helperText: helperText,
-      ),
+
+      decoration: InputDecoration(
+          hintText: hintText,
+          labelText: labelText,
+          helperText: helperText,
+          suffixIcon: suffixIcon == null ? null : const Icon(Icons.group),
+          icon: icon == null ? null : Icon(icon)),
     );
   }
 }
